b66e5096fd04808a5297bed71a27e0e3a4932934
rigizer <rigizer@gmail.com>
2021-03-13 20:00:43 +0900

    신규 프로젝트 생성

---
+++ itnews-crawler/src/main/java/pe/kr/itnewscrawler/ItnewsCrawlerApplication.java
@@ -0,0 +1,13 @@
+package pe.kr.itnewscrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItnewsCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItnewsCrawlerApplication.class, args);
+	}
+
+}
---
+++ itnews-crawler/src/test/java/pe/kr/itnewscrawler/ItnewsCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.kr.itnewscrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItnewsCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
